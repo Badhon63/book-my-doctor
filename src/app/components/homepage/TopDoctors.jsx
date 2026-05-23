394dd385f24aa6f017a8c0162264f23b032f69ac
Badhon63 <jianrobiul7898@gmail.com>
2026-05-23 19:36:59 +0600

    added top doctors section in homepage

--- src/app/components/homepage/TopDoctors.jsx
+++ src/app/components/homepage/TopDoctors.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import CardAllDoctors from "./CardAllDoctors";
 
-const TopDoctors = () => {
-  return <div>top doctors</div>;
+const TopDoctors = ({ topDoctors }) => {
+  return (
+    <div className="max-w-7xl mx-auto pt-16">
+      <p className="text-center text-4xl md:text-5xl font-bold text-gray-900 mb-4">
+        Top Rated Doctors
+      </p>
+      <p className=" text-center text-lg text-gray-600 mb-10">
+        Highly reviewed specialists ready to see you.
+      </p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+        {topDoctors.map((doctor) => (
+          <CardAllDoctors doctor={doctor} key={doctor.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TopDoctors;
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,16 @@ import Banner from "./components/homepage/Banner";
 import FAQ from "./components/homepage/FAQ";
 import Howitworks from "./components/homepage/Howitworks";
 import TopDoctors from "./components/homepage/TopDoctors";
+import { getAllDoctors } from "./lib/fetchData";
 
-export default function Home() {
+export default async function Home() {
+  const doctors = await getAllDoctors();
+  const topDoctors = doctors.slice(0, 3);
+
   return (
     <div>
       <Banner />
-      <TopDoctors />
+      <TopDoctors topDoctors={topDoctors} />
       <Howitworks />
       <FAQ />
     </div>
