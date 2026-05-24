8692c508823bf1e5d473cd7f3c6c1a93c75b3079
Badhon63 <jianrobiul7898@gmail.com>
2026-05-24 12:54:27 +0600

    added login authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,73 +3,100 @@
 import React from "react";
 import {
   Button,
-  Description,
   FieldError,
   Form,
   Input,
   Label,
   TextField,
 } from "@heroui/react";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+import { authClient } from "@/lib/auth-client";
 
 const Login = () => {
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const user = Object.fromEntries(formData.entries());
-    console.log(user);
+    console.log("user:", user);
+    const { data, error } = await authClient.signIn.email({
+      email: user.email,
+      password: user.password,
+      rememberMe: true,
+      callbackURL: "/",
+    });
+    console.log("data:", data, "error:", error);
   };
 
   return (
-    <div>
-      {" "}
-      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
-        <TextField
-          isRequired
-          name="email"
-          type="email"
-          validate={(value) => {
-            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-              return "Please enter a valid email address";
-            }
-            return null;
-          }}
-        >
-          <Label>Email</Label>
-          <Input placeholder="john@example.com" />
-          <FieldError />
-        </TextField>
-        <TextField
-          isRequired
-          minLength={8}
-          name="password"
-          type="password"
-          validate={(value) => {
-            if (value.length < 8) {
-              return "Password must be at least 8 characters";
-            }
-            if (!/[A-Z]/.test(value)) {
-              return "Password must contain at least one uppercase letter";
-            }
-            if (!/[0-9]/.test(value)) {
-              return "Password must contain at least one number";
-            }
-            return null;
-          }}
-        >
-          <Label>Password</Label>
-          <Input placeholder="Enter your password" />
-          <Description>
-            Must be at least 8 characters with 1 uppercase and 1 number
-          </Description>
-          <FieldError />
-        </TextField>
-        <div className="flex gap-2">
-          <Button type="submit">Submit</Button>
-          <Button type="reset" variant="secondary">
-            Reset
-          </Button>
+    <div className="max-w-7xl mx-auto flex justify-center mt-12">
+      <div className="flex flex-col max-w-sm lg:min-w-sm bg-gray-100 p-8 rounded-lg">
+        <div className="flex flex-col text-center">
+          <p className="font-bold text-4xl">Login</p>
+          <p className="text-gray-600 pt-2 pb-5">Welcome back to DoctorNow</p>
         </div>
-      </Form>
+        <Form className="flex  flex-col gap-4 rounded-lg" onSubmit={onSubmit}>
+          <TextField
+            isRequired
+            name="email"
+            type="email"
+            validate={(value) => {
+              if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                return "Please enter a valid email address";
+              }
+              return null;
+            }}
+          >
+            <Label>Email</Label>
+            <Input placeholder=" Emter your email" />
+            <FieldError />
+          </TextField>
+
+          <TextField
+            isRequired
+            minLength={8}
+            name="password"
+            type="password"
+            validate={(value) => {
+              if (value.length < 8) {
+                return "Password must be at least 8 characters";
+              }
+              if (!/[A-Z]/.test(value)) {
+                return "Password must contain at least one uppercase letter";
+              }
+              if (!/[0-9]/.test(value)) {
+                return "Password must contain at least one number";
+              }
+              return null;
+            }}
+          >
+            <Label>Password</Label>
+            <Input placeholder="Enter your password" />
+
+            <FieldError />
+          </TextField>
+          <Button type="submit" className={"w-full"}>
+            Login
+          </Button>
+        </Form>
+        <div className="flex justify-center items-center py-5 gap-4">
+          <div className="border h-px w-full"></div>
+          <div>OR</div> <div className="border h-px w-full"></div>
+        </div>
+        <div className="flex items-center select-none cursor-pointer gap-2 justify-center bg-white p-2 font-semibold rounded-lg">
+          <span className="text-xl">
+            {" "}
+            <FcGoogle />
+          </span>
+          <p>Login with Google</p>
+        </div>
+        <p className="text-sm text-center mt-5">
+          Don&apos;t have an account?{" "}
+          <span className="underline">
+            <Link href={"/signup"}>Register</Link>
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
